0202827df349fba5a5040ec6a6acb4926f3badbe
Zhehui Huang <zhehuihu@usc.edu>
2022-09-29 16:05:18 -0700

    Version 1

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+from result import *
+
+
+def esdf(M, N, obstacle_list):
+    """
+    :param M: Row number
+    :param N: Column number
+    :param obstacle_list: Obstacle list
+    :return: An array. The value of each cell means the closest distance to the obstacle
+    """
+    pass
+
+
+if __name__ == '__main__':
+    st = time.time()
+    for _ in range(int(2e4)):
+        assert np.array_equal(esdf(M=3, N=3, obstacle_list=[[0, 1], [2, 2]]), res_1)
+        assert np.array_equal(esdf(M=4, N=5, obstacle_list=[[0, 1], [2, 2], [3, 1]]), res_2)
+
+    et = time.time()
+    print(et-st)
---
+++ result.py
@@ -0,0 +1,6 @@
+import numpy as np
+res_1 = [[1, 0, 1], [np.sqrt(2), 1, 1], [2, 1, 0]]
+res_2 = [[1, 0, 1, 2, np.sqrt(8)],
+        [np.sqrt(2), 1, 1, np.sqrt(2), np.sqrt(5)],
+        [np.sqrt(2), 1, 0, 1, 2],
+        [1, 0, 1, np.sqrt(2), np.sqrt(5)]]
