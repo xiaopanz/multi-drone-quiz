4910d2b239bff4d7ccf9c3ca898c5c404821672d
xiaopanz <xiaopanz@umich.edu>
2022-10-08 23:05:07 -0700

    Problem 1 Implementation

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import time
 import math
 from result import *
 
-
+"""
+Implemented an efficient Euclidean Distance Transform algorthim from 
+https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.107.5775&rep=rep1&type=pdf
+"""
 def esdf(M, N, obstacle_list):
     """
     :param M: Row number
@@ -10,7 +13,7 @@ def esdf(M, N, obstacle_list):
     :param obstacle_list: Obstacle list
     :return: An array. The value of each cell means the closest distance to the obstacle
     """
-    # Create the distance grid with inf distance as initial value
+    # Create the distance grid with inf(999) distance as initial value
     distance_grid = [[999 for _ in range(N)] for _ in range(M)]
 
     # perform row scanning, firstly from left to right then from right to left
@@ -20,10 +23,11 @@ def esdf(M, N, obstacle_list):
                 distance_grid[i][j] = 0
             else:
                 distance_grid[i][j] = distance_grid[i][j - 1] + 1
+    # scan row from right to left
     for i in range(M):
         for j in range(N - 2, -1, -1):
             if [i, j] in obstacle_list:
-                distance_grid[i][j] = 0
+                continue
             else:
                 distance_grid[i][j] = min(distance_grid[i][j], distance_grid[i][j + 1] + 1)
 
@@ -38,46 +42,40 @@ def esdf(M, N, obstacle_list):
         start = 0
         for i in range(M):
             if distance_grid[i][j] < 999:
-                stack = [[i, i]]
+                stack = [[i, 0]]
                 start = i
                 break
-        for i in range(start, M):
+        for i in range(start + 1, M):
             if distance_grid[i][j] > 999:
                 continue
+            # calculate the intersections on row
             influ = i + (distance_grid[i][j] - distance_grid[stack[-1][0]][j] - np.square(i - stack[-1][0])) / (2 * (i - stack[-1][0]))
             if influ > M:
                 continue
             if influ > stack[-1][1]:
                 stack.append([i, influ])
             else:
-                stack.pop()
                 while len(stack):
                     influ = i + (distance_grid[i][j] - distance_grid[stack[-1][0]][j] - np.square(i - stack[-1][0])) / (2 * (i - stack[-1][0]))
-
                     if influ > stack[-1][1]:
                         break
                     else:
                         stack.pop()
                 stack.append([i, influ])
 
-        # perform second-pass column scanning by using saved stack
+        # perform second-pass column scanning by using stack
         prev = M
         for row, influence in reversed(stack):
-            if(math.ceil(influence+0.01) < 0):
-                for i in range(0, prev):
+            # scale up the intersection point
+            for i in range(max(0, math.ceil(influence+0.01)), prev):
+                if i < M:
                     distance_grid[i][j] = distance_grid[row][j] + np.square(row - i)
-                prev = 0
-            else:
-                for i in range(math.ceil(influence+0.01), prev):
-                    if i < M:
-                        distance_grid[i][j] = distance_grid[row][j] + np.square(row - i)
-                prev = math.ceil(influence+0.01)
-
-        for i in range(0,math.ceil(stack[0][1]+0.01)):
+                prev = max(0, math.ceil(influence+0.01))
+        for i in range(0, prev):
             if i < M:
                 distance_grid[i][j] = distance_grid[start][j] + np.square(start - i)
 
-
+    # calculate square root value for all distances
     for i in range(M):
         for j in range(N):
             distance_grid[i][j] = np.sqrt(distance_grid[i][j])
